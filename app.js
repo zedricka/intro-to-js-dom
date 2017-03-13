6ac9536263402a6af2ebd22de33daac35f0cc0aa
zedricka <zedrick.ara@gmail.com>
2017-03-13 08:33:35 -1000

    getElementsByClassName

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 console.log ("Sanity Check");
 
-// Finding HTML Elements
+// Finding HTML Elements (ID)
 // console.log(document.getElementById("use-id-method"));
 var idElement = document.getElementById("use-id-method"); //Giving it another name
 console.log(idElement);
 
-// Finding Tag names
-console.log(document.getElementsByTagName("script")); //Grabs all script tags
-console.log(document.getElementsByTagName("div")); //Grabs all div tags
+// Finding Tag Names
+console.log(document.getElementsByTagName("script")); //Grabs all script tags - Shows as array [script]
+console.log(document.getElementsByTagName("div")); //Grabs all div tags - Shows as array [div, div#use-id-method, div.use-class-me...]
+
+// Finding HTML Elements (Class)
+console.log(document.getElementsByClassName("use-class-method")); //Grabs "use-class-method" class element - Shows as array [div.use-class-method, div.use-class-method]
