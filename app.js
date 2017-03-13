a28fb32f08ee990e2ebcfb27b7037a5b850ae103
zedricka <zedrick.ara@gmail.com>
2017-03-13 08:28:25 -1000

    Updated

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log ("Sanity Check")
+console.log ("Sanity Check");
+
+// Finding HTML Elements
+// console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method"); //Giving it another name
+console.log(idElement);
+
+// Finding Tag names
+console.log(document.getElementsByTagName("script")); //Grabs all script tags
+console.log(document.getElementsByTagName("div")); //Grabs all div tags
